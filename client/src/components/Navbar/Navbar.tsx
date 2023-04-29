61fc341410a524d579b43ee0836415c0e5825180
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-04-29 14:26:04 -0400

    added a simle navbar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 shadow-sm backdrop-blur">
+      <div className="container flex h-14 items-center">Navbar</div>
+    </header>
+  );
+}
